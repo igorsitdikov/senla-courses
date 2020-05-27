8bf5f6d023b97c5e7c43fd9e29f78ce7e87f5f2f
Sitdzikau Ihar <ihar.sitdzikau@yandex.by>
2020-05-27 11:43:15 +0300

    updated product

--- task-3/3/src/Product.java
+++ task-3/3/src/Product.java
@@ -2,22 +2,25 @@ import interfaces.IProduct;
 import interfaces.IProductPart;
 
 public class Product implements IProduct {
+    private Monitor monitor;
+    private MotherBoard motherBoard;
+    private Body body;
 
     @Override
     public void installFirstPart(final IProductPart iProductPart) {
-        final Monitor monitor = (Monitor) iProductPart;
+        monitor = (Monitor) iProductPart;
         System.out.println(String.format("%s was installed", monitor.toString()));
     }
 
     @Override
     public void installSecondPart(final IProductPart iProductPart) {
-        final MotherBoard motherBoard = (MotherBoard) iProductPart;
+        motherBoard = (MotherBoard) iProductPart;
         System.out.println(String.format("%s was installed", motherBoard.toString()));
     }
 
     @Override
     public void installThirdPart(final IProductPart iProductPart) {
-        final Body body = (Body) iProductPart;
+        body = (Body) iProductPart;
         System.out.println(String.format("%s was installed", body.toString()));
     }
 }
