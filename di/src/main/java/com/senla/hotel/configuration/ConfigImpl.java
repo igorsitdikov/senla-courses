d9ec1163d4aa8727a83a01e1bd2d88c609bae464
Sitdzikau Ihar <ihar.sitdzikau@yandex.by>
2020-07-19 20:49:50 +0300

    removed Reflections lib;

--- di/src/main/java/com/senla/hotel/configuration/ConfigImpl.java
+++ di/src/main/java/com/senla/hotel/configuration/ConfigImpl.java
@@ -3,27 +3,33 @@ package com.senla.hotel.configuration;
 import com.senla.annotation.Singleton;
 import com.senla.hotel.Scanner;
 import com.senla.hotel.configuration.interfaces.Config;
-import org.reflections.Reflections;
 
 import java.util.Map;
+import java.util.Set;
 
 public class ConfigImpl implements Config {
-    private final Reflections scanner;
+    private final Scanner scanner;
     private Map<Class<?>, Class<?>> ifc2ImplClass;
 
     public ConfigImpl(final String packageToScan) {
-        this.ifc2ImplClass = Scanner.findInfImplInPackage(packageToScan, Singleton.class);
-        this.scanner = new Reflections(packageToScan);
+        this.scanner = new Scanner(packageToScan);
+        this.ifc2ImplClass = scanner.findInfImplInPackage(packageToScan, Singleton.class);
     }
 
     @Override
     @SuppressWarnings("unchecked")
     public <T> Class<T> getImplClass(final Class<T> ifc) {
-        return (Class<T>) ifc2ImplClass.get(ifc);
+        return (Class<T>) ifc2ImplClass.computeIfAbsent(ifc, aClass -> {
+            final Set<Class<?>> classes = scanner.getSubTypesOf(ifc);
+            if (classes.size() != 1) {
+                throw new RuntimeException(ifc + " has 0 or more than one impl please update your config");
+            }
+            return classes.iterator().next();
+        });
     }
 
     @Override
-    public Reflections getScanner() {
+    public Scanner getScanner() {
         return scanner;
     }
 }
