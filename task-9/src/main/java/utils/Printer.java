55194cef947c0ee46c51ed73e16dc1438d4f3690
Sitdzikau Ihar <ihar.sitdzikau@yandex.by>
2020-07-26 21:58:48 +0300

    completed task 4;

--- task-9/src/main/java/utils/Printer.java
+++ task-9/src/main/java/utils/Printer.java
@@ -1,11 +1,21 @@
 package utils;
 
+import java.time.LocalDateTime;
+
 public class Printer {
     public static void printName(final Thread t) {
         System.out.printf("Thread name : %s%n", t.getName());
     }
 
-    public static void printException(final Thread t, Exception e) {
+    public static void printException(final Thread t, final Exception e) {
         System.err.printf("%s - %s %n", t.getName(), e.getMessage());
+    }
+
+    public static void printState(final String msg, final Thread t) {
+        System.out.printf("%s - %s state: %s%n", msg, t.getName(), t.getState());
+    }
+
+    public static void printDateTime() {
+        System.out.println(LocalDateTime.now());
     }
 }
