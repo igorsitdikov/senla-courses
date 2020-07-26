3b3d6da4332889cc8861421f6381153d83b79a5e
Sitdzikau Ihar <ihar.sitdzikau@yandex.by>
2020-07-26 21:58:48 +0300

    completed task 4;

---
+++ task-9/src/main/java/forth/ThreadDaemon.java
@@ -0,0 +1,14 @@
+package forth;
+
+public class ThreadDaemon {
+    public static void main(final String[] args) {
+        final Integer sec = 5;
+        final DateTimePrinter printer = new DateTimePrinter(sec);
+        printer.setDaemon(true);
+        printer.start();
+
+        while (true) {
+
+        }
+    }
+}
