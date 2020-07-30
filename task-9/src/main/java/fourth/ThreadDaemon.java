6f957a1eaa348e08b0337117ec72302f92612fb1
sitdikov <s@m.ru>
2020-07-30 12:25:00 +0300

    updated Task-9-2 and Task-9-4;

--- task-9/src/main/java/fourth/ThreadDaemon.java
+++ task-9/src/main/java/fourth/ThreadDaemon.java
@@ -1,14 +1,17 @@
 package fourth;
 
+import utils.Printer;
+
 public class ThreadDaemon {
     public static void main(final String[] args) {
         final Integer sec = 5;
         final DateTimePrinter printer = new DateTimePrinter(sec);
         printer.setDaemon(true);
         printer.start();
-
-        while (true) {
-
+        try {
+            Thread.sleep(sec * 100);
+        } catch (InterruptedException e) {
+            Printer.printException(printer, e);
         }
     }
 }
