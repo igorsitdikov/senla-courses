0a1f767aba3097661a432261a22b27c76c197332
sitdikov <s@m.ru>
2020-10-07 10:36:56 +0300

    fix: unit RoomHistoryService tests; updated RoomHistoryDtoMapperImpl;

--- model/src/main/java/com/senla/hotel/mapper/RoomHistoryDtoMapperImpl.java
+++ model/src/main/java/com/senla/hotel/mapper/RoomHistoryDtoMapperImpl.java
@@ -1,20 +1,19 @@
 package com.senla.hotel.mapper;
 
+import com.senla.hotel.dto.ResidentDto;
+import com.senla.hotel.dto.RoomDto;
 import com.senla.hotel.dto.RoomHistoryDto;
+import com.senla.hotel.entity.Resident;
+import com.senla.hotel.entity.Room;
 import com.senla.hotel.entity.RoomHistory;
-import com.senla.hotel.mapper.interfaces.dtoMapper.ResidentDtoMapper;
-import com.senla.hotel.mapper.interfaces.dtoMapper.RoomDtoMapper;
 import com.senla.hotel.mapper.interfaces.dtoMapper.RoomHistoryDtoMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class RoomHistoryDtoMapperImpl implements RoomHistoryDtoMapper {
-
-    @Autowired
-    private ResidentDtoMapper residentDtoMapper;
-    @Autowired
-    private RoomDtoMapper roomDtoMapper;
 
     @Override
     public RoomHistory sourceToDestination(final RoomHistoryDto source) {
@@ -22,8 +21,8 @@ public class RoomHistoryDtoMapperImpl implements RoomHistoryDtoMapper {
         destination.setId(source.getId());
         destination.setCheckOut(source.getCheckOut());
         destination.setCheckIn(source.getCheckIn());
-        destination.setResident(residentDtoMapper.sourceToDestination(source.getResidentDto()));
-        destination.setRoom(roomDtoMapper.sourceToDestination(source.getRoomDto()));
+        destination.setResident(sourceResidentToDestinationResident(source.getResidentDto()));
+        destination.setRoom(sourceRoomToDestinationRoom(source.getRoomDto()));
         destination.setStatus(source.getStatus());
         return destination;
     }
@@ -34,9 +33,72 @@ public class RoomHistoryDtoMapperImpl implements RoomHistoryDtoMapper {
         source.setId(destination.getId());
         source.setCheckOut(destination.getCheckOut());
         source.setCheckIn(destination.getCheckIn());
-        source.setResidentDto(residentDtoMapper.destinationToSource(destination.getResident()));
-        source.setRoomDto(roomDtoMapper.destinationToSource(destination.getRoom()));
+        source.setResidentDto(destinationResidentToSourceResident(destination.getResident()));
+        source.setRoomDto(destinationRoomToSourceRoom(destination.getRoom()));
         source.setStatus(destination.getStatus());
         return source;
+    }
+
+    private Resident sourceResidentToDestinationResident(final ResidentDto source) {
+        Resident destination = new Resident();
+        destination.setId(source.getId());
+        destination.setFirstName(source.getFirstName());
+        destination.setLastName(source.getLastName());
+        destination.setGender(source.getGender());
+        destination.setPhone(source.getPhone());
+        destination.setVip(source.getVip());
+        return destination;
+    }
+
+    private ResidentDto destinationResidentToSourceResident(final Resident destination) {
+        ResidentDto source = new ResidentDto();
+        source.setId(destination.getId());
+        source.setFirstName(destination.getFirstName());
+        source.setLastName(destination.getLastName());
+        source.setGender(destination.getGender());
+        source.setPhone(destination.getPhone());
+        source.setVip(destination.getVip());
+        return source;
+    }
+
+    private Room sourceRoomToDestinationRoom(final RoomDto source) {
+        Room destination = new Room();
+        destination.setId(source.getId());
+        destination.setPrice(source.getPrice());
+        destination.setStatus(source.getStatus());
+        destination.setAccommodation(source.getAccommodation());
+        destination.setNumber(source.getNumber());
+        destination.setStars(source.getStars());
+        return destination;
+    }
+
+    private RoomDto destinationRoomToSourceRoom(final Room destination) {
+        RoomDto source = new RoomDto();
+        source.setId(destination.getId());
+        source.setPrice(destination.getPrice());
+        source.setStatus(destination.getStatus());
+        source.setAccommodation(destination.getAccommodation());
+        source.setNumber(destination.getNumber());
+        source.setStars(destination.getStars());
+        source.setHistoriesDto(historyEntitiesToListDto(destination.getHistories()));
+        return source;
+    }
+
+    private List<RoomHistoryDto> historyEntitiesToListDto(List<RoomHistory> histories) {
+        List<RoomHistoryDto> roomHistoryDtos = new ArrayList<>();
+        for (int i = 0; i < histories.size(); i++) {
+            roomHistoryDtos.add(entityToDto(histories.get(i)));
+        }
+        return roomHistoryDtos;
+    }
+
+    private RoomHistoryDto entityToDto(RoomHistory roomHistory) {
+        RoomHistoryDto roomHistoryDto = new RoomHistoryDto();
+        roomHistoryDto.setId(roomHistory.getId());
+        roomHistoryDto.setCheckOut(roomHistory.getCheckOut());
+        roomHistoryDto.setCheckIn(roomHistory.getCheckIn());
+        roomHistoryDto.setResidentDto(destinationResidentToSourceResident(roomHistory.getResident()));
+        roomHistoryDto.setStatus(roomHistory.getStatus());
+        return roomHistoryDto;
     }
 }
--- model/src/test/java/com/senla/hotel/service/RoomHistoryServiceImplTest.java
+++ model/src/test/java/com/senla/hotel/service/RoomHistoryServiceImplTest.java
@@ -1,130 +1,152 @@
-//package com.senla.hotel.service;
-//
-//import com.senla.hotel.HotelTest;
-//import com.senla.hotel.dao.interfaces.RoomHistoryDao;
-//import com.senla.hotel.dto.ResidentDto;
-//import com.senla.hotel.dto.RoomDto;
-//import com.senla.hotel.dto.RoomHistoryDto;
-//import com.senla.hotel.entity.Resident;
-//import com.senla.hotel.entity.Room;
-//import com.senla.hotel.entity.RoomHistory;
-//import com.senla.hotel.enumerated.Accommodation;
-//import com.senla.hotel.enumerated.Gender;
-//import com.senla.hotel.enumerated.HistoryStatus;
-//import com.senla.hotel.enumerated.RoomStatus;
-//import com.senla.hotel.enumerated.Stars;
-//import com.senla.hotel.exceptions.EntityNotFoundException;
-//import com.senla.hotel.exceptions.PersistException;
-//import com.senla.hotel.mapper.AttendanceDtoMapperImpl;
-//import com.senla.hotel.mapper.ResidentDtoMapperImpl;
-//import com.senla.hotel.mapper.RoomDtoMapperImpl;
-//import com.senla.hotel.mapper.RoomHistoryDtoMapperImpl;
-//import com.senla.hotel.mapper.interfaces.dtoMapper.AttendanceDtoMapper;
-//import com.senla.hotel.mapper.interfaces.dtoMapper.ResidentDtoMapper;
-//import com.senla.hotel.mapper.interfaces.dtoMapper.RoomDtoMapper;
-//import com.senla.hotel.mapper.interfaces.dtoMapper.RoomHistoryDtoMapper;
-//import com.senla.hotel.service.interfaces.RoomHistoryService;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.Spy;
-//import org.mockito.junit.jupiter.MockitoExtension;
-//import org.springframework.test.context.ContextConfiguration;
-//import org.springframework.test.context.junit.jupiter.SpringExtension;
-//import org.springframework.test.context.web.WebAppConfiguration;
-//
-//import java.math.BigDecimal;
-//import java.time.LocalDate;
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//import static org.mockito.ArgumentMatchers.any;
-//import static org.mockito.BDDMockito.given;
-//import static org.mockito.Mockito.times;
-//import static org.mockito.Mockito.verify;
-//import static org.springframework.test.util.AssertionErrors.assertEquals;
-//
-//@ExtendWith(SpringExtension.class)
-//@ExtendWith(MockitoExtension.class)
-//@WebAppConfiguration
-//@ContextConfiguration(classes = HotelTest.class)
-//public class RoomHistoryServiceImplTest {
-//
-//    @Mock
-//    private RoomHistoryDao roomHistoryDao;
-//    @Spy
-//    private RoomHistoryDtoMapper roomHistoryDtoMapper = new RoomHistoryDtoMapperImpl();
-//    @Spy
-//    private ResidentDtoMapper residentDtoMapper = new ResidentDtoMapperImpl();
-//    @Spy
-//    private AttendanceDtoMapper attendanceDtoMapper = new AttendanceDtoMapperImpl();
-//    @Spy
-//    private RoomDtoMapper roomDtoMapper = new RoomDtoMapperImpl();
-//    @InjectMocks
-//    private RoomHistoryService roomHistoryService = new RoomHistoryServiceImpl();
-//
-//    @Test
-//    void findRoomHistoryByIdTest() throws PersistException, EntityNotFoundException {
-//        final Long roomHistoryId = 1L;
-//        final Room room101 =
-//            new Room(101, Stars.STANDARD, Accommodation.SGL, BigDecimal.valueOf(100), RoomStatus.VACANT);
-//        final Resident mike = new Resident("Mike", "Smith", Gender.MALE, false, "1234567", null);
-//
-//        final RoomHistory roomHistory =
-//            new RoomHistory(room101, mike, LocalDate.of(2020, 8, 3), LocalDate.of(2020, 8, 5),
-//                            HistoryStatus.CHECKED_IN);
-//        given(roomHistoryDao.findById(roomHistoryId)).willReturn(roomHistory);
-//        RoomHistoryDto actual = roomHistoryService.findById(roomHistoryId);
-//
-//        assertEquals("find by id result: ", roomHistory, actual);
-//        verify(roomHistoryDao, times(1)).findById(roomHistoryId);
-//    }
-//
-//    @Test
-//    void shouldReturnFindAllTest() throws PersistException {
-//        List<RoomHistory> roomHistories = new ArrayList<>();
-//        final Room room101 =
-//            new Room(101, Stars.STANDARD, Accommodation.SGL, BigDecimal.valueOf(100), RoomStatus.VACANT);
-//        final Resident mike = new Resident("Mike", "Smith", Gender.MALE, false, "1234567", null);
-//
-//        final RoomHistory roomHistory1 =
-//            new RoomHistory(room101, mike, LocalDate.of(2020, 8, 3), LocalDate.of(2020, 8, 5),
-//                            HistoryStatus.CHECKED_OUT);
-//        final RoomHistory roomHistory2 =
-//            new RoomHistory(room101, mike, LocalDate.of(2020, 9, 3), LocalDate.of(2020, 9, 5),
-//                            HistoryStatus.CHECKED_IN);
-//
-//        roomHistories.add(roomHistory1);
-//        roomHistories.add(roomHistory2);
-//
-//        given(roomHistoryDao.getAll()).willReturn(roomHistories);
-//        List<RoomHistoryDto> expected = roomHistoryService.showHistories();
-//        assertEquals("find all result: ", expected, roomHistories);
-//
-//        verify(roomHistoryDao, times(1)).getAll();
-//    }
-//
-//
-//    @Test
-//    void createRoomHistoryTest() throws PersistException {
-//        final RoomDto room101 = new RoomDto();
-//        room101.setId(1L);
-//        room101.setNumber(101);
-//        room101.setStars(Stars.STANDARD);
-//        room101.setAccommodation(Accommodation.SGL);
-//        room101.setPrice(BigDecimal.valueOf(100));
-//        room101.setStatus(RoomStatus.VACANT);
-//        final ResidentDto mike = new ResidentDto("Mike", "Smith", Gender.MALE, false, "1234567", null);
-//
-//        final RoomHistoryDto roomHistory =
-//            new RoomHistoryDto(room101, mike, LocalDate.of(2020, 8, 3), LocalDate.of(2020, 8, 5),
-//                               HistoryStatus.CHECKED_IN);
-//
-//        given(roomHistoryService.create(roomHistory)).willReturn(roomHistory);
-//
-//        roomHistoryService.create(roomHistory);
-//
-//        verify(roomHistoryDao, times(1)).create(any(RoomHistory.class));
-//    }
-//}
+package com.senla.hotel.service;
+
+import com.senla.hotel.HotelTest;
+import com.senla.hotel.dao.interfaces.RoomHistoryDao;
+import com.senla.hotel.dto.ResidentDto;
+import com.senla.hotel.dto.RoomDto;
+import com.senla.hotel.dto.RoomHistoryDto;
+import com.senla.hotel.entity.Resident;
+import com.senla.hotel.entity.Room;
+import com.senla.hotel.entity.RoomHistory;
+import com.senla.hotel.enumerated.Accommodation;
+import com.senla.hotel.enumerated.Gender;
+import com.senla.hotel.enumerated.HistoryStatus;
+import com.senla.hotel.enumerated.RoomStatus;
+import com.senla.hotel.enumerated.Stars;
+import com.senla.hotel.exceptions.EntityNotFoundException;
+import com.senla.hotel.exceptions.PersistException;
+import com.senla.hotel.mapper.AttendanceDtoMapperImpl;
+import com.senla.hotel.mapper.ResidentDtoMapperImpl;
+import com.senla.hotel.mapper.RoomDtoMapperImpl;
+import com.senla.hotel.mapper.RoomHistoryDtoMapperImpl;
+import com.senla.hotel.mapper.interfaces.dtoMapper.AttendanceDtoMapper;
+import com.senla.hotel.mapper.interfaces.dtoMapper.ResidentDtoMapper;
+import com.senla.hotel.mapper.interfaces.dtoMapper.RoomDtoMapper;
+import com.senla.hotel.mapper.interfaces.dtoMapper.RoomHistoryDtoMapper;
+import com.senla.hotel.service.interfaces.RoomHistoryService;
+import mock.ResidentMock;
+import mock.RoomMock;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.util.AssertionErrors.assertEquals;
+
+@ExtendWith(SpringExtension.class)
+@ExtendWith(MockitoExtension.class)
+@WebAppConfiguration
+@ContextConfiguration(classes = HotelTest.class)
+public class RoomHistoryServiceImplTest {
+
+    @Mock
+    private RoomHistoryDao roomHistoryDao;
+    @Spy
+    private RoomHistoryDtoMapper roomHistoryDtoMapper = new RoomHistoryDtoMapperImpl();
+    @InjectMocks
+    private RoomHistoryService roomHistoryService = new RoomHistoryServiceImpl();
+
+    @Test
+    void findRoomHistoryByIdTest() throws PersistException, EntityNotFoundException {
+        final Long roomHistoryId = 1L;
+        final Long roomId = 1L;
+        final Long residentId = 1L;
+        final RoomDto roomDto = RoomMock.getDtoById(roomId);
+        final Room room = RoomMock.getById(roomId);
+        final ResidentDto residentDto = ResidentMock.getDtoById(residentId);
+        final Resident resident = ResidentMock.getById(residentId);
+
+        final RoomHistory roomHistory =
+                new RoomHistory(room, resident, LocalDate.of(2020, 8, 3), LocalDate.of(2020, 8, 5),
+                        HistoryStatus.CHECKED_IN);
+        final RoomHistoryDto expected =
+                new RoomHistoryDto(null, roomDto, residentDto, new ArrayList<>(), LocalDate.of(2020, 8, 3), LocalDate.of(2020, 8, 5),
+                        HistoryStatus.CHECKED_IN);
+        given(roomHistoryDao.findById(roomHistoryId)).willReturn(roomHistory);
+        RoomHistoryDto actual = roomHistoryService.findById(roomHistoryId);
+
+        assertEquals("find by id result: ", expected, actual);
+        verify(roomHistoryDao, times(1)).findById(roomHistoryId);
+    }
+
+    @Test
+    void shouldReturnFindAllTest() throws PersistException {
+        List<RoomHistory> roomHistories = new ArrayList<>();
+        List<RoomHistoryDto> expected = new ArrayList<>();
+        final Long roomHistoryId = 1L;
+        final Long roomId = 1L;
+        final Long residentId = 1L;
+        final RoomDto roomDto = RoomMock.getDtoById(roomId);
+        final Room room = RoomMock.getById(roomId);
+        final ResidentDto residentDto = ResidentMock.getDtoById(residentId);
+        final Resident resident = ResidentMock.getById(residentId);
+
+        final RoomHistory roomHistory1 =
+                new RoomHistory(room, resident, LocalDate.of(2020, 8, 3), LocalDate.of(2020, 8, 5),
+                        HistoryStatus.CHECKED_OUT);
+        final RoomHistory roomHistory2 =
+                new RoomHistory(room, resident, LocalDate.of(2020, 9, 3), LocalDate.of(2020, 9, 5),
+                        HistoryStatus.CHECKED_IN);
+        final RoomHistoryDto expected1 =
+                new RoomHistoryDto(null, roomDto, residentDto, new ArrayList<>(), LocalDate.of(2020, 8, 3), LocalDate.of(2020, 8, 5),
+                        HistoryStatus.CHECKED_OUT);
+        final RoomHistoryDto expected2 =
+                new RoomHistoryDto(null, roomDto, residentDto, new ArrayList<>(), LocalDate.of(2020, 9, 3), LocalDate.of(2020, 9, 5),
+                        HistoryStatus.CHECKED_IN);
+        expected.add(expected1);
+        expected.add(expected2);
+        roomHistories.add(roomHistory1);
+        roomHistories.add(roomHistory2);
+
+        given(roomHistoryDao.getAll()).willReturn(roomHistories);
+        List<RoomHistoryDto> actual = roomHistoryService.showHistories();
+        assertEquals("find all result: ", expected.size(), 2);
+        assertEquals("find all result: ", expected.size(), actual.size());
+        assertEquals("find all result: ", expected.get(0), actual.get(0));
+        assertEquals("find all result: ", expected.get(1), actual.get(1));
+
+        verify(roomHistoryDao, times(1)).getAll();
+    }
+
+    @Test
+    void createRoomHistoryTest() throws PersistException {
+        final Long roomId = 1L;
+        final Long residentId = 1L;
+        final RoomDto roomDto = RoomMock.getDtoById(roomId);
+        final Room room = RoomMock.getById(roomId);
+        final ResidentDto residentDto = ResidentMock.getDtoById(residentId);
+        final Resident resident = ResidentMock.getById(residentId);
+
+        final RoomHistory roomHistory =
+                new RoomHistory(room, resident, LocalDate.of(2020, 8, 3), LocalDate.of(2020, 8, 5),
+                        HistoryStatus.CHECKED_OUT);
+        final RoomHistoryDto expected =
+                new RoomHistoryDto(null, roomDto,
+                        residentDto, new ArrayList<>(),
+                        LocalDate.of(2020, 8, 3),
+                        LocalDate.of(2020, 8, 5),
+                        HistoryStatus.CHECKED_OUT);
+
+        given(roomHistoryDao.create(any(RoomHistory.class))).willReturn(roomHistory);
+
+        final RoomHistoryDto actual = roomHistoryService.create(expected);
+        assertEquals("find all result: ", expected, actual);
+        verify(roomHistoryDao, times(1)).create(any(RoomHistory.class));
+    }
+}
