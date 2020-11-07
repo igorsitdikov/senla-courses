17084d90310d3c9ffe34f319bb0ad732a57261b5
Sitdzikau Ihar <ihar.sitdzikau@yandex.by>
2020-11-07 10:51:34 +0300

    added project name as package;

--- application/src/main/java/com/senla/bulletin_board/application/BulletinBoardApplication.java
+++ application/src/main/java/com/senla/bulletin_board/application/BulletinBoardApplication.java
@@ -1,11 +1,11 @@
-package com.senla.application;
+package com.senla.bulletin_board.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.senla"})
+@ComponentScan(basePackages = {"com.senla.bulletin_board"})
 public class BulletinBoardApplication {
 
     public static void main(String[] args) {
--- configuration/src/main/java/com/senla/bulletin_board/configuration/ApiBasePathConfiguration.java
+++ configuration/src/main/java/com/senla/bulletin_board/configuration/ApiBasePathConfiguration.java
@@ -1,4 +1,4 @@
-package com.senla.configuration;
+package com.senla.bulletin_board.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
