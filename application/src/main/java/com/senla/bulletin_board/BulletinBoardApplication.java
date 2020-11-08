6374a4b66e7d8bbdde3ead78d28ca28e540388af
Sitdzikau Ihar <ihar.sitdzikau@yandex.by>
2020-11-08 22:31:19 +0300

    removed PackageScanner from BulletinBoardApplication;

--- application/src/main/java/com/senla/bulletin_board/BulletinBoardApplication.java
+++ application/src/main/java/com/senla/bulletin_board/BulletinBoardApplication.java
@@ -1,11 +1,9 @@
-package com.senla.bulletin_board.application;
+package com.senla.bulletin_board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.senla.bulletin_board"})
 public class BulletinBoardApplication {
 
     public static void main(String[] args) {
